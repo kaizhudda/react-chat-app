8276c6c7cdf0da483c90e6eef566802d89968728
Kaiz Hudda <kaiz.hudda@gmail.com>
2020-04-13 10:00:59 +1200

    redirect to app if isLoggedIn

--- src/index.js
+++ src/index.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import * as serviceWorker from "./serviceWorker";
+import firebase from "./firebase";
 import "semantic-ui-css/semantic.min.css";
 
 const Root = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+  const history = useHistory();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        history.push("/");
+        setIsLoggedIn(true);
+        console.log(history);
+      }
+    });
+  }, []);
   return (
     <Router>
       <Switch>
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/" component={App} />
+        {isLoggedIn ? (
+          <Route path="/">
+            <App />
+          </Route>
+        ) : (
+          <>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <App />
+            </Route>
+          </>
+        )}
       </Switch>
     </Router>
   );
 };
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <Root />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
